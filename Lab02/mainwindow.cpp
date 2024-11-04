c71f6f1bb914e9038e85c901a142440f3ec2c980
yuerbufei <3094906739@qq.com>
2024-11-04 17:06:22 +0800

    5)新建文件、另存文件、保存文件、打开文件操作

--- Lab02/mainwindow.cpp
+++ Lab02/mainwindow.cpp
@@ -67,6 +67,125 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+// 新建文件
+void MainWindow::on_actionNew_triggered()
+{
+    if (!userEditConfirmed()) {
+        return;
+    }
+
+    filePath.clear();  // 清空当前文件路径
+    ui->textEdit->clear();  // 清除文本编辑器内容
+    setWindowTitle(tr("无标题 - 编辑器"));  // 设置窗口标题
+    textChanged = false;  // 重置文本更改标志
+}
+// 打开文件
+void MainWindow::on_actionOpen_triggered()
+{
+    if (!userEditConfirmed()) {
+        return;
+    }
+
+    QString filename = QFileDialog::getOpenFileName(this, tr("打开文件"), ".", tr("Text files (*.txt);;All files (*.*)"));
+
+    if (filename.isEmpty()) {
+        return;  // 如果用户取消了选择，则直接返回
+    }
+
+    QFile file(filename);
+    if (!file.open(QFile::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("错误"), tr("无法打开文件"));
+        return;
+    }
+
+    filePath = filename;
+    QTextStream in(&file);
+    ui->textEdit->setPlainText(in.readAll());
+    file.close();
+
+    setWindowTitle(QFileInfo(filePath).fileName() + " - 编辑器");  // 更新窗口标题
+    textChanged = false;  // 重置文本更改标志
+}
+// 保存文件
+void MainWindow::on_actionSave_triggered()
+{
+    if (filePath.isEmpty()) {
+        on_actionSaveAs_triggered();  // 如果没有文件路径，就当作另存为
+    } else {
+        QFile file(filePath);
+        if (!file.open(QFile::WriteOnly | QFile::Text)) {
+            QMessageBox::warning(this, tr("错误"), tr("无法保存文件"));
+            return;
+        }
+
+        QTextStream out(&file);
+        out << ui->textEdit->toPlainText();
+        file.flush();
+        file.close();
+
+        textChanged = false;  // 保存后重置文本更改标志
+    }
+}
+
+// 确认用户是否要保存更改
+bool MainWindow::userEditConfirmed()
+{
+    if (textChanged) {
+        QString path = !filePath.isEmpty() ? filePath : "无标题.txt";
+
+        QMessageBox msg(this);
+        msg.setIcon(QMessageBox::Question);
+        msg.setWindowTitle(tr("新建文件"));
+        msg.setText(tr("是否将更改保存到\n") + "\"" + path + "\"?");
+        msg.setStandardButtons(QMessageBox::Yes | QMessageBox::No | QMessageBox::Cancel);
+
+        int r = msg.exec();
+        switch (r) {
+        case QMessageBox::Yes:
+            on_actionSave_triggered();
+            break;
+        case QMessageBox::No:
+            textChanged = false;
+            break;
+        case QMessageBox::Cancel:
+            return false;
+        default:
+            break;
+        }
+    }
+
+    return true;
+}
+// 另存为
+void MainWindow::on_actionSaveAs_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, tr("另存为"), ".", tr("Text files (*.txt);;All files (*.*)"));
+
+    if (filename.isEmpty()) {
+        return;  // 用户取消保存
+    }
+
+    QFile file(filename);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("错误"), tr("无法保存文件"));
+        return;
+    }
+
+    filePath = filename;
+    QTextStream out(&file);
+    out << ui->textEdit->toPlainText();
+    file.flush();
+    file.close();
+
+    setWindowTitle(QFileInfo(filePath).fileName() + " - 编辑器");  // 更新窗口标题
+    textChanged = false;  // 保存后重置文本更改标志
+}
+// 退出
+void MainWindow::on_actionExit_triggered()
+{
+    qApp->exit(0);
+}
+
 // 撤销操作
 void MainWindow::on_actionUndo_triggered()
 {
